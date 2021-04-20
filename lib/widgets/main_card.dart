884568fa432e004ab7bd6ae48bfe421428f2de63
Tính Phan <tinhpt.38@gmail.com>
2021-04-20 17:50:33 +0700

    add main card

---
+++ lib/widgets/main_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainCard extends StatefulWidget {
+  @override
+  _MainCardState createState() => _MainCardState();
+}
+
+class _MainCardState extends State<MainCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
