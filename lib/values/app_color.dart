68ee000c093997ac2eee5cf8e4ef9af6f17a434d
Tính Phan <tinhpt.38@gmail.com>
2021-04-20 17:23:55 +0700

    project structure

---
+++ lib/values/app_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color black = Color(0xff4A4A4A);
+  static final Color blue = Color(0xff4A90E2);
+  static final Color grey = Color(0xffC1C0C9);
+  static final Color red = Color(0xffE21F70);
+  static final Color orgain = Color(0xffFF4D2C);
+  static final Color white = Color(0xfffffff);
+  static final Color cyan = Color(0xff12CBC4);
+  static final Color yellow = Color(0xffFFC312);
+  static final Color violet = Color(0xffD980FA);
+  static final Color pink = Color(0xffFDA7DF);
+  static final Color green = Color(0xffA3CB38);
+}
